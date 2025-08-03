59b37d81dcb216a72a3a1218230cad812974cb69
Sarah Davis <170495238+SarahDavis2@users.noreply.github.com>
2025-08-03 17:33:12 -0500

    Add operate
    Connects calculator operator functions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+let num1 = 0;
+let num2 = 0;
+let operator = '';
+
+function operate(num1, operator, num2) {
+    switch (operator) {
+        case '+':
+            let sum = add(num1, num2);
+            break;
+        case '-':
+            let difference = subtract(num1, num2);
+            break;
+        case '*':
+            let product = multiply(num1, num2);
+            break;
+        case '/':
+            let quotient = divide(num1, num2);
+            break;
+    }
+}
+
 // CALCULATOR OPERATOR FUNCTIONS
 function add(num1, num2) {
     return num1 + num2;
